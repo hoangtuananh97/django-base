0026d54bdf884a384ffe077b11c3e83b241b651e
hoangtuananh <hoangtuananh1997tb@gmail.com>
2022-11-09 12:36:06 +0700

    Fix ci.yml

--- manage.py
+++ manage.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
-        from django.core.management import execute_from_command_line
+        from app.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django  # noqa
+            import app  # noqa
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
